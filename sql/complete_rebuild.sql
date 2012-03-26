6fa76377f6bd666d570a9a4c0038feb7370d3bb6
Alex Weber <alexwebr@gmail.com>
2012-03-26 07:45:40 -0500

    Reorganized CREATE TABLE and CREATE FUNCTION statements to make FKs and column references valid

--- sql/complete_rebuild.sql
+++ sql/complete_rebuild.sql
@@ -12,11 +12,61 @@ drop table port_detail_type;
 drop table host_detail_type;
 
 
+
+-- HOST TABLES
+-- Types of detailed, unstructured information about a host
+create table host_detail_types (
+ id BIGSERIAL PRIMARY KEY,
+ name varchar(100) NOT NULL,
+ searchable boolean NOT NULL
+);
+
+-- Hosts table - IP address and last-seen date (should maybe be in details?)
+create table hosts (
+ id BIGSERIAL PRIMARY KEY,
+ ip INET NOT NULL, -- INET holds network and host IPv6 and IPv4 addresses
+ last_seen DATE 
+);
+
+-- Detailed, unstructured information about a host - we need to create this last, otherwise its FKs don't exist :)
+create table host_details (
+ id BIGSERIAL PRIMARY KEY,
+ host_id REFERENCES hosts(id) ON DELETE CASCADE NOT NULL,
+ type REFERENCES host_detail_type(id) NOT NULL ON DELETE CASCADE,
+ text TEXT NOT NULL -- TEXT = "unlimited" length
+);
+
+
+-- PORTS/SERVICES TABLES
+-- Ports table detail types
+create table port_detail_types (
+ id BIGSERIAL PRIMARY KEY,
+ name varchar(100) NOT NULL,
+ searchable boolean NOT NULL
+);
+
+-- Ports table - this needs to be created last, otherwise the FKs don't exist
+create table ports (
+ id BIGSERIAL PRIMARY KEY,
+ host_id REFERENCES hosts(id) ON DELETE CASCADE NOT NULL,
+ port NUMBER(8),
+ transport_protocol varchar(8)
+);
+
+-- Ports table details - SSL? Service Version?
+create table port_details (
+ id BIGSERIAL PRIMARY KEY,
+ port_id REFERENCES ports(id) ON DELETE CASCASE NOT NULL,
+ type REFERENCES port_detail_types(id) ON DELETE CASCADE NOT NULL,
+ text TEXT NOT NULL -- TEXT = "unlimited" length text
+);
+
+
 -- FUNCTIONS AND TRIGGERS
 -- Function to force uppercasing of protocols...
 create function upper_protocol() returns trigger as '
  begin
- NEW.protocol := upper(NEW.protocol);
+ NEW.transport_protocol := upper(NEW.transport_protocol);
  return NEW;
  end;
 ' language 'plpgsql';
@@ -26,38 +76,3 @@ create trigger upper_protocol
  after insert on ports
  for each row execute procedure upper_protocol();
 
-
--- TABLE DECLARATIONS
--- Hosts table
-create table hosts (
- id BIGSERIAL PRIMARY KEY,
- ip INET NOT NULL, -- INET holds network and host IPv6 and IPv4 addresses
- last_seen DATE 
-);
-
--- Detailed, unstructured information about a host
-create table host_details (
- id BIGSERIAL PRIMARY KEY,
- host_id REFERENCES hosts(id) ON DELETE CASCADE NOT NULL,
- type REFERENCES host_detail_type(id) NOT NULL ON DELETE CASCADE;
- text TEXT NOT NULL -- TEXT = "unlimited" length
-);
-
--- Types of detailed, unstructured information about a host
-create table host_detail_types (
- id BIGSERIAL PRIMARY KEY,
- name varchar(100) NOT NULL,
- searchable boolean NOT NULL
-);
-
--- Ports table
-create table ports (
- id BIGSERIAL PRIMARY KEY,
- host_id REFERENCES hosts(id) ON DELETE CASCADE NOT NULL,
- port NUMBER(8)
- transport_protocol varchar(8),
-);
-
--- TODO Ports table detail types
-
--- TODO Ports table details - SSL? Service Version?
